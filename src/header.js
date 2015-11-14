a81cf1fc32b7c415a12bf338ac0abcc481e734cf
tsuyoshiwada <v23.b.wada@gmail.com>
2015-11-15 00:43:55 +0900

    Add license header

---
+++ src/header.js
@@ -0,0 +1,9 @@
+/*!
+ * cake-hash
+ * CakePHP in Utility.Hash class like a collection manipulation. In JavaScript.
+ * 
+ * @author tsuyoshiwada
+ * @homepage https://github.com/tsuyoshiwada/cake-hash
+ * @license MIT
+ * @version 0.0.1
+ */
